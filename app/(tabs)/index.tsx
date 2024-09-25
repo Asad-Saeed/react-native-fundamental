cff8e923f9f2de2c62ab74055d82654fbf370857
Asad-gsoft-consulting <[200~Asad-gsoft-consulting>
2024-09-25 22:48:58 +0500

    feat:scrollview component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, ImageBackground } from "react-native";
+import { Image, StyleSheet, ImageBackground, ScrollView } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -9,15 +9,16 @@ const logo = require("@/assets/images/icon.png");
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
+    <ScrollView>
+      {/* <ParallaxScrollView
+          headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+          headerImage={
+            <Image
+              source={require("@/assets/images/partial-react-logo.png")}
+              style={styles.reactLogo}
+            />
+          }
+    > */}
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome! Asad Saeed</ThemedText>
         <HelloWave />
@@ -52,7 +53,35 @@ export default function HomeScreen() {
           </ThemedText>
         </ImageBackground>
       </ThemedView>
-    </ParallaxScrollView>
+      <ThemedText style={styles.text}>static image</ThemedText>
+      <Image source={logo} style={styles.image} />
+      <ThemedText style={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quidem
+        magni cum, deleniti veritatis porro! Atque tempora temporibus iure,
+        facere aspernatur nesciunt deserunt rerum illum, magni velit nemo,
+        distinctio veniam consequatur! Quos, libero culpa similique minus
+        tempore nostrum eum sit perspiciatis aut accusantium vel in ipsa
+        asperiores architecto voluptatum animi a magni expedita inventore
+        accusamus optio! Laborum repudiandae adipisci maxime tenetur id facilis
+        totam soluta quasi dicta quidem! Adipisci ad consequuntur quis
+        dignissimos vitae error debitis? Provident aspernatur iste dolore
+        obcaecati laborum ullam quaerat praesentium dolor earum, nemo, quisquam
+        excepturi maiores dolorum repudiandae adipisci natus deserunt expedita
+        explicabo, rem cupiditate. Natus, iusto. Dolores, fugiat quis autem id
+        veniam sequi nostrum enim. Deserunt assumenda necessitatibus, officiis
+        temporibus tempora non nesciunt fugit, eum impedit libero ab eius. Ipsum
+        sapiente, rem ipsa maiores, fugiat doloribus cupiditate blanditiis,
+        labore reprehenderit ab odit libero. Maxime ducimus perferendis, magnam
+        dignissimos quas repudiandae similique culpa rerum. Quos magnam numquam
+        maxime doloribus voluptates recusandae accusamus et rerum ipsam illum
+        possimus suscipit repellendus error, corporis aliquid excepturi odio
+        fugiat quia, officiis at. Libero aliquam sunt vitae amet dolore nisi
+        nihil necessitatibus laboriosam ipsum architecto. Sequi fugiat cum quasi
+        consequuntur. Necessitatibus dolorum nulla aperiam eius aspernatur
+        doloribus repellat iste? Nobis.
+      </ThemedText>
+      {/* </ParallaxScrollView> */}
+    </ScrollView>
   );
 }
 
