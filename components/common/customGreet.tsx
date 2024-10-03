72bf455ec2288e211e30388fd4f96586078014eb
Asad-gsoft-consulting <[200~Asad-gsoft-consulting>
2024-10-03 10:21:09 +0500

    feat:stylesheet api + multiple styles

--- components/common/customGreet.tsx
+++ components/common/customGreet.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 const CustomGreetComp = ({ name }: any) => {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>Hello {name}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello {name}</Text>
+      <View style={[styles.box, styles.lightBlueBox]}>
+        <Text style={styles.text}>Hello {name}</Text>
+      </View>
+      <View style={[styles.box, styles.lightGreenBox]}>
+        <Text style={styles.text}>Hello {name}</Text>
+      </View>
     </View>
   );
 };
 
 export default CustomGreetComp;
+
+// Style Sheet
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "yellow", padding: 60 },
+  title: { fontSize: 30, color: "pink" },
+  text: { color: "red" },
+  // extract styles
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    // in array last style have precedence
+    backgroundColor: "pink",
+  },
+  lightBlueBox: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBox: {
+    backgroundColor: "lightgreen",
+  },
+});
