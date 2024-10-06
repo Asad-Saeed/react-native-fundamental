92d649d6577ad78429aa2858003cc04b17497464
Asad-gsoft-consulting <[200~Asad-gsoft-consulting>
2024-10-06 12:02:02 +0500

    feat:dimensions api drawback solve with subscriptions

--- components/common/dimensionsApi.tsx
+++ components/common/dimensionsApi.tsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 const DimensionsApi = () => {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+  useLayoutEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription.remove();
+  }, []);
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+  console.log("Dimensions", { windowHeight, windowWidth });
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        {/* <Text style={styles.text}>Welcome</Text> */}
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome</Text>
       </View>
     </View>
   );
 };
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-console.log("Dimensions", { windowHeight, windowWidth });
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
+// console.log("Dimensions", { windowHeight, windowWidth });
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,12 +46,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: { fontSize: windowWidth > 300 ? 50 : 20 },
+  // text: { fontSize: windowWidth > 300 ? 50 : 20 },
 });
 export default DimensionsApi;
